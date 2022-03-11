3ee69b5c499dde69fd2566a075ab355b80841ede
KaylaCanada <kmc0099@mix.wvu.edu>
2022-03-11 08:03:42 -0500

    Kayla Canada Task 08

--- Task07/Task07.R
+++ Task07/Task07.R
@@ -4,7 +4,14 @@ library(auk)
 states<-c("US-NY", "CR-P")
 system.file("extdata/ebd-sample.txt",package="auk") %>%
   auk_ebd() %>%
-  auk_species(species="Blue Jay") %>%
+  auk_state(states)
+ebd<-auk_ebd(system.file("extdata/ebd-smaple.txt", package="auk"))
+system.file("extdata/ebd-sample.txt", package="auk")%>%
+ auk_ebd()%>%
+
+f<-system.file("extdata/ebd-sample.txt", package="auk")
+ebd<-auk_ebd(f)%>%
+auk_species(species="Blue Jay", ) %>%
   auk_country(country="United States") %>%
   auk_state(state="New York") %>%
   auk_filter(file = output_file) %>%
---
+++ Task08/Task08.R
@@ -0,0 +1,46 @@
+library(phytools)
+library(ape)
+text.string<-
+  "(((((((cow, pig), whale), (bat, (lemur, human))), (robin, iguana)), coelacanth), (gold_fish, trout)), shark);"
+vert.tree<-read.tree(text=text.string)
+plot(vert.tree, edge.width=2)
+nodelabels(frame="circle", bg='white', cex=1)
+Question#1 Ibelieve that the shark is more closely related to the goldfish. There are less nodes in comparison to the distance between the human and goldfish.
+vert.tree
+Question#2 There are no branch lengths in this tree. 
+str(vert.tree)
+tree<-read.tree(text="(((A,B), (C,D)), E);")
+plotTree(tree, offset=1)
+tiplabels(frame="circle", bg='lightblue', cex=1)
+nodelabels(frame="circle", bg='white', cex=1)
+tree$tip.label
+tree$edge
+AnolisTree<-force.ultrametric(read.tree("https://jonsmitchell.com/data/anolis.tre"))
+par(las=1)
+hist(AnolisTree$edge.length, col='black', border='white', main="", xlab="edge lengths for the Anolis tree", ylim=c(0, 50), xlim=c(0,6))
+tipEdges<-which(AnolisTree$edge[,2]<=Ntip(AnolisTree))
+Lengths<-AnolisTree$edge.length
+names(Lengths)<-AnolisTree$tip.label
+names(Lengths)[which(Lengths == min(Lengths))]
+plot(AnolisTree, cex=0.25)
+Labs<-sapply(AnolisTree$edge.length, round, digits=2)
+edgelabels(text=Labs, cex=0.25)
+?plot.phylo
+#Question3: plot(AnolisTree, tip.label=FALSE)
+#Question 4: plot(AnolisTree, cex=0.25, type='fan')
+warnings()
+#Question 5: plot(AnolisTree, cex=0.25, tip.color='red')
+#Question 6:
+plot(AnolisTree, cex=0.25)
+Labs<-sapply(AnolisTree$edge.length, round, digits=2)
+edgelabels(text=Labs, cex=0.25)
+which(Lengths == min(Lengths))
+names(Lengths)
+AnolisTree2<-drop.tip(AnolisTree, "Anolis_occultus")
+plot(AnolisTree2, cex=0.25)
+ltt(AnolisTree)
+abline(0, 1, lwd=2, col='red', lty=2)
+#Question 9: The slope on the graph does not appear to ever go down. I believe this to be because population of the Anolis lizards are reproducing without signs extinction. 
+#Question 10: Rate Is on line 46
+fit.bd(AnolisTree, b, d, rho=0.2)
+(b/lambda=0.8031), (d/mu=0),and log(L)=132.9163
